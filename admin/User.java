1c87de9664c6fb9db35b225be69bebd9725d7b2c
AbdullahBabsail <103104609+AbdullahBabsail@users.noreply.github.com>
2022-10-25 16:22:14 +0300

    Create User.java

---
+++ admin/User.java
@@ -0,0 +1,45 @@
+package com.example2.admin;
+
+public class User {
+    private String usernmae , fullname, email, phonenumber , banned;
+
+    public String getBanned() {
+        return banned;
+    }
+
+    public void setBanned(String banned) {
+        this.banned = banned;
+    }
+
+    public String getUsernmae() {
+        return usernmae;
+    }
+
+    public void setUsernmae(String usernmae) {
+        this.usernmae = usernmae;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+}
