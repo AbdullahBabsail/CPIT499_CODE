089d0d3e780607ae5cdb87c0fa8a42cee5c9a0b2
AbdullahBabsail <103104609+AbdullahBabsail@users.noreply.github.com>
2022-10-25 16:20:43 +0300

    Create Announcement.java

---
+++ admin/Announcement.java
@@ -0,0 +1,56 @@
+package com.example2.admin;
+
+public class Announcement {
+
+    private String Type , Time ,Date ,Details, Location , Owner;
+
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getDetails() {
+        return Details;
+    }
+
+    public void setDetails(String details) {
+        Details = details;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+}
+
